4307d4487fcd4a6c987bedf79cb77efc16942814
Nika (Ionut Cojanu) <cojanuionut9@gmail.com>
2026-02-04 19:01:45 +0100

    chore: practice

--- ToC/Exercises/ex2/ex2.c
+++ ToC/Exercises/ex2/ex2.c
@@ -1,28 +1,16 @@
 #include <stdio.h>
 
-int main(void);
+int main(void)
 {
-    int a, b, e;
-    float c,d;
+    int var1, var2;
+    float var3, var4;
+    
+    printf("var1: %d\n", var1);
+    printf("var2: %d\n", var2);
 
-    printf("Insert number: ");
-    scanf("%d" , &a);
-    printf("Insert number: ");
-    scanf("%d" , &b);
-
-    printf("Insert number: ");
-    scanf("%f" , c);
-    printf("Insert number: ");
-    scanf("%f" , d);
-
-    printf("Select operation:\n");
-    printf("1 for addition\n");
-    printf("2 for substraction\n");
-    printf("3 for division\n");
-    printf("4 for multiplication");
-    scanf("%d" , e);
+    printf("var3: %f\n", var3);
+    printf("var4: %f\n", var4);
 
     return 0;
-
 
 }
