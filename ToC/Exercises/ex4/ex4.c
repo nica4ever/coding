3472ef07209d476f5846c9f3e8fa7fc614ffc2d6
Nika/Cojanu Ionut <cojanuionut9@gmail.com>
2026-02-07 20:01:06 +0100

    chore: exrcise 4

--- ToC/Exercises/ex4/ex4.c
+++ ToC/Exercises/ex4/ex4.c
@@ -5,4 +5,11 @@
 
 int main (void)
 {
+    float pi = 3.14159f, ra = 10 , ra3 , vol;
+
+    ra3 = ra * ra * ra;
+    vol = 4.0f/3.0f * pi * ra3;
+    printf("Volume of a sphere with a 10 meter radius(4/3πr^3): %.2f\n", vol);
     
+    return 0;
+}    
