1132955e5208b3c0446059bcc5d647f8b4bf7b3d
Nika / Cojanu Ionut <cojanuionut9@gmail.com>
2026-03-27 22:26:17 +0200

    feat: practice

---
+++ Python/100/1.18.py
@@ -0,0 +1,5 @@
+# 1.18 Create a string "ERROR". Print it padded to 10 characters, left-aligned.
+#      Then print it right-aligned in 10 characters.
+#      Hint: f-string formatting with < and >
+string = "ERROR"
+
