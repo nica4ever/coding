7ea3f580b40db50d9a5791efe2238bf73c9d4ec0
Nika / Cojanu Ionut <cojanuionut9@gmail.com>
2026-03-29 18:27:35 +0300

    feat: Practice

---
+++ Python/100/TOPIC_3/3.1.py
@@ -0,0 +1,7 @@
+# 3.1  Create a dict with 3 key-value pairs: name, age, city. Print the dict.
+dic = {
+        "name": "Alex",
+        "age": "26",
+        "city": "Amsterdam",
+        }
+print(dic)
---
+++ Python/100/TOPIC_3/3.2.py
@@ -0,0 +1,8 @@
+# 3.2  Create a dict. Access one value by key. Print it.
+dic = {
+        "name": "Alex",
+        "age": "26",
+        "city": "Amsterdam",
+        }
+print(dic["name"])
+
